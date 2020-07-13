1e69e74edd777cb8d10bd95f5664e31b3db773d6
Ryan Chung <ryanchuung@gmail.com>
2020-07-13 15:20:18 -0700

    added logout view/template

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
 ]
