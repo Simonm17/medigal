90b5e58084f3431fd72f070479e8865dd95acea3
Ryan Chung <ryanchuung@gmail.com>
2020-07-15 10:28:48 -0700

    added pw change view/htmls with url routing

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -8,4 +8,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='users/change_password.html'), name='change-password'),
+    path('password-changedone/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+
+
 ]
