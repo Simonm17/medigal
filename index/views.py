19f886f54fe30b5f5308b9d5e866cc572401e986
Ryan Chung <ryanchuung@gmail.com>
2020-07-15 07:39:00 -0700

    deleted timezone, added datetime view/html

--- index/views.py
+++ index/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+import datetime
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'index/home.html', context={})
+    return render(request, 'index/home.html', context={
+        'timezone': timezone.now(),  # only using if wanting to display UTC timezone
+        'datetime': datetime.datetime.now()  # displays current local timezone
+    })
