773b663c82fcba44b559464b771c613f29bc513b
Ryan Chung <ryanchuung@gmail.com>
2020-07-15 19:59:53 -0700

    fully functional password reset system. need styling

--- medigal/settings.py
+++ medigal/settings.py
@@ -125,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# For password reset email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('MEDIGAL_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('MEDIGAL_EMAIL_PASS')
