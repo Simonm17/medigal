eee85ef16752b8e3386617b85c97d2e537034497
Ryan Chung <ryanchuung@gmail.com>
2020-07-27 14:48:52 -0700

    added email/tele update views with next params on detailview

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AddressDetailView, AddressUpdateView
+from .views import AddressDetailView, AddressUpdateView, TelephoneUpdateView, EmailUpdateView
 
 urlpatterns = [
-    path('<int:pk>/', AddressDetailView.as_view(), name='address_detail'),
-    path('<int:pk>/update/', AddressUpdateView.as_view(), name='address_update'),
+    path('address/<int:pk>/', AddressDetailView.as_view(), name='address_detail'),
+    path('address/<int:pk>/update/', AddressUpdateView.as_view(), name='address_update'),
+    path('telephone/<int:pk>/update/', TelephoneUpdateView.as_view(), name='telephone_update'),
+    path('email/<int:pk>/update/', EmailUpdateView.as_view(), name='email_update'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 from users.models import User
 from .models import Address, Telephone, Email
 from doctors.models import Doctor
+from applicants.models import Applicant
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -21,7 +22,14 @@ class AddressUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['doctor'] = Doctor.objects.all()
+        context['applicant'] = Applicant.objects.all()
+        context['next_url'] = self.request.GET.get('next') # pass `next` parameter received from previous page to the context
         return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
 
 
 class TelephoneDetailView(DetailView):
@@ -30,5 +38,53 @@ class TelephoneDetailView(DetailView):
 class TelephoneUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Telephone
     fields = '__all__'
-    template_name = 'contact/telephone_update.html'
+    template_name = 'contacts/telephone_update.html'
     success_message = 'Telephone number has been updated successfully!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctor'] = Doctor.objects.all()
+        context['applicant'] = Applicant.objects.all()
+        context['next_url'] = self.request.GET.get('next') # pass `next` parameter received from previous page to the context
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+
+class EmailUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Email
+    fields = '__all__'
+    template_name = 'contacts/email_update.html'
+    success_message = 'Email has been updated successfully!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctor'] = Doctor.objects.all()
+        context['applicant'] = Applicant.objects.all()
+        context['next_url'] = self.request.GET.get('next') # pass `next` parameter received from previous page to the context
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+        
+class TelephoneUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Telephone
+    fields = '__all__'
+    template_name = 'contacts/telephone_update.html'
+    success_message = 'Telephone has been updated successfully!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctor'] = Doctor.objects.all()
+        context['applicant'] = Applicant.objects.all()
+        context['next_url'] = self.request.GET.get('next') # pass `next` parameter received from previous page to the context
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
