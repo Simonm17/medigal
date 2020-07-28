50bfdbcfdc5a0967f8ebf78b89a9ccfef25916e8
Ryan Chung <ryanchuung@gmail.com>
2020-07-28 02:05:57 -0700

    added createview/url/templates for email/tele

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import AddressDetailView, AddressUpdateView, TelephoneUpdateView, EmailUpdateView
+from .views import AddressCreateView, AddressDetailView, AddressUpdateView, TelephoneCreateView, TelephoneUpdateView, EmailCreateView, EmailUpdateView
 
 urlpatterns = [
+    path('address/create/', AddressCreateView.as_view(), name='address_create'),
     path('address/<int:pk>/', AddressDetailView.as_view(), name='address_detail'),
     path('address/<int:pk>/update/', AddressUpdateView.as_view(), name='address_update'),
+
+    path('telephone/create/', TelephoneCreateView.as_view(), name='telephone_create'),
     path('telephone/<int:pk>/update/', TelephoneUpdateView.as_view(), name='telephone_update'),
+
+    path('email/create/', EmailCreateView.as_view(), name='email_create'),
     path('email/<int:pk>/update/', EmailUpdateView.as_view(), name='email_update'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -5,10 +5,27 @@ from doctors.models import Doctor
 from applicants.models import Applicant
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
+class AddressCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Address
+    fields = '__all__'
+    template_name = 'contacts/address_create.html'
+    success_message = 'Address has been created successfully!'
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next_url'] = self.request.GET.get('next')
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            print(f'CUSTOM: next_url = {next_url}')
+            return next_url
 
 class AddressDetailView(DetailView):
     model = Address
@@ -29,8 +46,27 @@ class AddressUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     def get_success_url(self):
         next_url = self.request.GET.get('next')
         if next_url:
+            print(f'CUSTOM: next_url = {next_url}')
             return next_url
 
+
+
+class TelephoneCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Telephone
+    fields = '__all__'
+    template_name = 'contacts/telephone_create.html'
+    success_message = 'Telephone has been created successfully!'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next_url'] = self.request.GET.get('next')
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
 
 class TelephoneDetailView(DetailView):
     model = Telephone
@@ -46,6 +82,24 @@ class TelephoneUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
         context['doctor'] = Doctor.objects.all()
         context['applicant'] = Applicant.objects.all()
         context['next_url'] = self.request.GET.get('next') # pass `next` parameter received from previous page to the context
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+
+
+class EmailCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Address
+    fields = '__all__'
+    template_name = 'contacts/email_create.html'
+    success_message = 'Email has been created successfully!'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next_url'] = self.request.GET.get('next')
         return context
 
     def get_success_url(self):
